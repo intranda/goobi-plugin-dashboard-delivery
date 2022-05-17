2f52853404b833b24ad69ded6ed1304ab1115a2e
Robert Sehr <robert.sehr@intranda.com>
2022-05-17 10:01:47 +0200

    show welcome screen

--- plugin/src/main/java/de/intranda/goobi/plugins/DeliveryDashboardPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/DeliveryDashboardPlugin.java
@@ -1,10 +1,11 @@
 package de.intranda.goobi.plugins;
 
+import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginType;
 import org.goobi.production.plugin.interfaces.IDashboardPlugin;
 
-import de.sub.goobi.config.ConfigPlugins;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 
@@ -17,19 +18,23 @@ public class DeliveryDashboardPlugin implements IDashboardPlugin {
 
     @Getter
     private PluginType type = PluginType.Dashboard;
-    
+
     @Getter
     private String guiPath = "/uii/plugin_dashboard_delivery.xhtml";
-    
+
     @Getter
-    private String value;
+    @Setter
+    private String navigation = "main";
+
+    @Getter
+    private PluginGuiType pluginGuiType = PluginGuiType.FULL;
 
     /**
      * Constructor
      */
     public DeliveryDashboardPlugin() {
         log.info("Delivery dashboard plugin started");
-        value = ConfigPlugins.getPluginConfig(title).getString("value", "default value");
-    }   
+        //        value = ConfigPlugins.getPluginConfig(title).getString("value", "default value");
+    }
 
 }
